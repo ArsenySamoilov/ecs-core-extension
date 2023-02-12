ba222614f8e984cc25c5fc1675ca89ad4bb9fc34
Arseny Samoilov <samoilov.arseny@gmail.com>
2023-02-12 09:49:56 +0300

    Add Systems
    For removing specified components and removing entities with specified component.

---
+++ Runtime/Systems/RemoveComponentSystem.cs
@@ -0,0 +1,19 @@
+﻿namespace SemsamECS.Core.Extension
+{
+    public sealed class RemoveComponentSystem<TComponent> : ISystem, IExecuteSystem where TComponent : struct
+    {
+        private readonly IPool<TComponent> _pool;
+
+        public RemoveComponentSystem(IWorld world)
+        {
+            _pool = world.Pools.Get<TComponent>();
+        }
+
+        public void Execute()
+        {
+            var entities = _pool.GetEntities();
+            for (var i = entities.Length - 1; i > -1; --i)
+                _pool.Remove(entities[i]);
+        }
+    }
+}
---
+++ Runtime/Systems/RemoveEntityWithComponentSystem.cs
@@ -0,0 +1,21 @@
+﻿namespace SemsamECS.Core.Extension
+{
+    public sealed class RemoveEntityWithComponentSystem<TComponent> : ISystem, IExecuteSystem where TComponent : struct
+    {
+        private readonly IEntities _entities;
+        private readonly IPool<TComponent> _pool;
+
+        public RemoveEntityWithComponentSystem(IWorld world)
+        {
+            _entities = world.Entities;
+            _pool = world.Pools.Get<TComponent>();
+        }
+
+        public void Execute()
+        {
+            var entities = _pool.GetEntities();
+            for (var i = entities.Length - 1; i > -1; --i)
+                _entities.Remove(entities[i]);
+        }
+    }
+}
